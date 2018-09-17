6c1dc6f8247b8277fe04bae668ead28813b32598
aliq03 <aliq03@hotmail.com>
2018-09-17 13:44:27 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+﻿var toolbarOptions = [
+  ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+  ['blockquote', 'code-block'],
+
+  [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+  [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+
+  [{ 'color': [] }],
+  [{ 'align': [] }],
+  
+  ['image', 'video'],// remove formatting button
+];
+var quill = new Quill('#editor', {
+    theme: 'snow',
+     modules: {
+        imageResize: {},
+        toolbar: toolbarOptions
+    }
+});
